4ca39a9e9bfbfbb2a513f13a2f1a0d0d4b54c8ef
juspoppa <152357749+juspoppa@users.noreply.github.com>
2024-04-18 12:47:42 -0400

    Progress bar added

--- Recipe.py
+++ Recipe.py
@@ -1,14 +1,21 @@
+import sys
+
 import requests
 from PyQt5.QtGui import QPixmap
+import urllib
+
 
 class Recipe:
-    def __init__(self, name, cook_time, prep_time, recipe_yield, image_url):
+    def __init__(self, name, cook_time, prep_time, recipe_yield, description, ingredients, total):
         self.name = name
         self.cook_time = cook_time
         self.prep_time = prep_time
         self.recipe_yield = recipe_yield
-        self.image_url = image_url
+        self.image = None
         self.image_filename = ""
+        self.description = description
+        self.ingredients = ingredients
+        self.total = total
 
     def get_name(self):
         return self.name
@@ -27,15 +34,20 @@ class Recipe:
         hours, minutes = divmod(time, 60)
         return f"{hours:02}:{minutes:02}"
 
-    def set_image(self, url):
-        # Image download logic with progress bar
-        response = requests.get(url, stream=True)
-        if response.status_code == 200:
-            with open(self.image_filename, 'wb') as f:
-                f.write(response.content)
-            print(f"! Downloading image {self.image_filename}")
-        else:
-            print(f"Failed to download {url}")
+    def set_image(self, url, index):
+        slash = url.rfind('/')
+        if slash != -1:
+            # Extract the substring after the last '/' to get the name of the file in the url
+            self.image_filename = url[slash + 1:]
+
+        # Checking for valid urls 
+        try:
+            response = requests.get(url, stream=True)
+            if response.status_code == 200:
+                with open(self.image_filename, 'wb') as f:
+                    print(f'/ Validating Recipe image {index + 1} of {self.total}')
+        except requests.exceptions.RequestException as e:
+            pass
 
     def get_image(self):
         return self.image_filename
