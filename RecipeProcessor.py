5126e48eaf817a6450a121d704d42a5fd4bb02ef
juspoppa <152357749+juspoppa@users.noreply.github.com>
2024-04-18 12:45:42 -0400

    Update RecipeProcessor.py to work with the Progress bar n Recipe.py

--- RecipeProcessor.py
+++ RecipeProcessor.py
@@ -9,10 +9,12 @@ class RecipeProcessor:
     def load_recipes(self, json_file):
         with open(json_file, 'r') as file:
             data = json.load(file)
-            for entry in data:
+            Recipe.recipes = len(data)
+            for index, entry in enumerate(data):
                 recipe = Recipe(entry['name'], entry['cookTime'], entry['prepTime'], entry['recipeYield'],
-                                entry['description'], entry['ingredients'], entry['image'])
+                                entry['description'], entry['ingredients'], len(data))
                 self._recipes.append(recipe)
+                recipe.set_image(entry['image'], index)
 
     def get_recipes(self):
         return self._recipes
